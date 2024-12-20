04e9e8af655f8447f39741f87fed96acb805d598
Russilx <sanmartinesteban123@gmail.com>
2024-12-20 02:30:49 -0300

    ASASASSA

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -17,12 +17,12 @@ const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, com
   for (const mem of participants) {
     teks += `┣➥ @${mem.id.split('@')[0]}\n`;
   }
-  teks += `*└* 𝐁𝐲 𝐓𝐡𝐞 𝐌𝐲𝐬𝐭𝐢𝐜 - 𝐁𝐨𝐭\n\n*▌│█║▌║▌║║▌║▌║▌║█*`;
+  teks += `*└* 𝐁𝐚𝐫𝐤𝐁𝐨𝐭 - @Tebi_.01`;
   conn.sendMessage(m.chat, {text: teks, mentions: participants.map((a) => a.id)} );
 };
 handler.help = ['tagall <mesaje>', 'invocar <mesaje>'];
 handler.tags = ['group'];
-handler.command = /^(tagall|invocar|invocacion|todos|invocación)$/i;
+handler.command = /^(todos)$/i;
 handler.admin = true;
 handler.group = true;
 export default handler;
